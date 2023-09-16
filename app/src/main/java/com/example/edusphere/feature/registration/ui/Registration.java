ca1f939df0bbe2742d45baba55e8405a80d2d66f
amirh <amirhazeev60189@gmail.com>
2023-09-17 00:16:58 +0300

    creating registration

--- app/src/main/java/com/example/edusphere/feature/registration/ui/Registration.java
+++ app/src/main/java/com/example/edusphere/feature/registration/ui/Registration.java
@@ -1,6 +1,9 @@
 package com.example.edusphere.feature.registration.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,10 +12,15 @@ import com.example.edusphere.R;
 import com.example.edusphere.databinding.ActivityRegistrationBinding;
 import com.example.edusphere.feature.registration.presentation.RegistrationViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Registration extends AppCompatActivity {
     private ActivityRegistrationBinding binding;
     private RegistrationViewModel viewModel;
+    private List<String> userTypes;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,55 @@ public class Registration extends AppCompatActivity {
 
         viewModel = new ViewModelProvider(this).get(RegistrationViewModel.class);
 
+        userTypes = new ArrayList<>();
+        userTypes.add("Студент");
+        userTypes.add("Учитель");
+        userTypes.add("Родитель");
+
+        adapter =new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, userTypes);
+
+        binding.typesList.setPrompt("Выберите помещение");
+        binding.typesList.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        binding.typesList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                renderFields(adapter.getItem(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         setContentView(binding.getRoot());
+    }
+
+    private void renderFields(String user){
+        switch (user){
+            case "Студент":
+                binding.parentLogin.setVisibility(View.VISIBLE);
+                binding.groupForStudent.setVisibility(View.VISIBLE);
+
+                binding.subject.setVisibility(View.GONE);
+                binding.schoolCode.setVisibility(View.GONE);
+                break;
+            case "Учитель":
+                binding.parentLogin.setVisibility(View.GONE);
+                binding.groupForStudent.setVisibility(View.GONE);
+
+                binding.subject.setVisibility(View.VISIBLE);
+                binding.schoolCode.setVisibility(View.VISIBLE);
+                break;
+            case "Родитель":
+                binding.parentLogin.setVisibility(View.GONE);
+                binding.groupForStudent.setVisibility(View.GONE);
+
+                binding.subject.setVisibility(View.GONE);
+                binding.schoolCode.setVisibility(View.GONE);
+                break;
+        }
     }
 }
