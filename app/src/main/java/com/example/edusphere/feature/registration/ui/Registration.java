aa8ec5222e8324ab780293a0c058e494309ed0c8
bulat <bulatkhaev@gmail.com>
2023-09-16 18:58:11 +0300

    added registration, updaded colors and design

---
+++ app/src/main/java/com/example/edusphere/feature/registration/ui/Registration.java
@@ -0,0 +1,17 @@
+package com.example.edusphere.feature.registration.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.edusphere.R;
+
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
