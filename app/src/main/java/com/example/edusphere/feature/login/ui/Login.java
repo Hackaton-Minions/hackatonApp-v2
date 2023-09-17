7325d3fa931c3e7a64de37a033860f483c9004eb
amirh <amirhazeev60189@gmail.com>
2023-09-17 05:22:04 +0300

    mnogo chego

--- app/src/main/java/com/example/edusphere/feature/login/ui/Login.java
+++ app/src/main/java/com/example/edusphere/feature/login/ui/Login.java
@@ -1,11 +1,14 @@
 package com.example.edusphere.feature.login.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.edusphere.databinding.ActivityLoginBinding;
+import com.example.edusphere.feature.registration.ui.Registration;
 
 
 public class Login extends AppCompatActivity {
@@ -15,7 +18,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
 
-
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), Registration.class));
+            }
+        });
         setContentView(binding.getRoot());
     }
 }
